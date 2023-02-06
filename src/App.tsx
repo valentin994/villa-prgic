4e752d7b51f9e42bea459da762ea8f99127013fe
valentin994 <valentin.vareskic@gmail.com>
2023-02-06 17:23:01 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import Nav from "./Components/Nav"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  )
+}
+
+export default App
