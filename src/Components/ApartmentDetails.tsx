c4533a35c175cc7c873010d5869c608fca0743c7
valentin994 <valentin.vareskic@gmail.com>
2023-02-10 14:35:13 +0100

    added state to the router link

--- src/Components/ApartmentDetails.tsx
+++ src/Components/ApartmentDetails.tsx
@@ -1,7 +1,12 @@
+import {useLocation} from "react-router-dom"
+
 function AparmentDetails() {
+    const { state }  = useLocation() 
+    const { name, detail, features, price } = state
+
     return (
-        <div>
-            <h1>Bla bla</h1>
+        <div className="container border-4">
+           <p className="text-3xl text-center font-bold">{name}</p>
         </div>
            )
 }
