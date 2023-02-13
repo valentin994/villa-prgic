fb0bb4e3b46606f7ccf239cc3b1811b02f93a83d
valentin994 <valentin.vareskic@gmail.com>
2023-02-13 13:56:05 +0100

    added calendar

--- src/Components/ApartmentDetails.tsx
+++ src/Components/ApartmentDetails.tsx
@@ -1,4 +1,7 @@
+import {useState} from "react";
 import {useLocation} from "react-router-dom"
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 function AparmentDetails() {
     const { state }  = useLocation() 
@@ -6,7 +9,46 @@ function AparmentDetails() {
 
     return (
         <div className="container border-4">
-           <p className="text-3xl text-center font-bold">{name}</p>
+           <p className="text-3xl text-center font-bold py-4">{name}</p>
+           <section className="overflow-hidden text-gray-700">
+              <div className="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
+                <div className="flex flex-wrap -m-1 md:-m-2">
+                  <div className="flex flex-wrap w-1/2">
+                    <div className="w-1/2 p-1 md:p-2">
+                      <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp" />
+                    </div>
+                    <div className="w-1/2 p-1 md:p-2">
+                      <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp" />
+                    </div>
+                    <div className="w-full p-1 md:p-2">
+                      <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap w-1/2">
+                    <div className="w-full p-1 md:p-2">
+                      <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp" />
+                    </div>
+                    <div className="w-1/2 p-1 md:p-2">
+                      <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
+                    </div>
+                    <div className="w-1/2 p-1 md:p-2">
+                      <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <p className="text-l text-center">{detail}</p>
+            <ul className="p-4">
+                {features.map((feature: string[]) => <li>{feature}</li>)}
+            </ul>
+            <Calendar />
         </div>
            )
 }
