6afa595b5cc8a9f4614baf664108b06dabd27d25
valentin994 <valentin.vareskic@gmail.com>
2023-09-13 15:53:26 +0200

    fixed link to homepage

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -7,9 +7,9 @@ function Nav() {
     return(    
         <nav className="border-gray-200 text-white rounded bg-secondary">
           <div className="flex flex-wrap items-center justify-between mx-auto w-full ">
-            <a href="#" className="flex items-center p-4">
+            <Link to="/" className="flex items-center p-4">
                 <span className="self-center text-xl font-light whitespace-nowrap">Villa Prgic</span>
-            </a>
+            </Link>
             <button data-collapse-toggle="navbar-solid-bg" onClick={() => setIsMenuOpen(!isMenuOpen)} type="button" className="inline-flex items-center p-4 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-solid-bg" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
               <svg className="w-6 h-6" aria-hidden="true" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
