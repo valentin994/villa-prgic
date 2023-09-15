e6453bda4e944fffdab17590c450105fd9e527d3
Valentin Vareskic <valentin.vareskic@gmail.com>
2023-09-15 14:52:04 +0200

    aligned home page, added eslint and prettier config

--- src/Components/ApartmentCard.tsx
+++ src/Components/ApartmentCard.tsx
@@ -1,25 +1,31 @@
-import { iApartment } from "../interfaces"
-import { Link, Outlet } from "react-router-dom"
+import { iApartment } from "../interfaces";
+import { Link } from "react-router-dom";
 import { ReactComponent as BedSvg } from "../assets/bed_svg.svg";
 
-function AparmentCard({name, detail, features, price }: iApartment) {
-    return (
+function AparmentCard({ name, detail, features, price }: iApartment) {
+  return (
     <div>
-        <Link to={name} state={{name, detail, features, price }} className="flex flex-col my-8 py-4 items-center bg-white border border-gray-200 bg-gray-100 hover:bg-gray-100">
-            <div className="flex flex-col justify-between p-4 leading-normal">
-                <div className="flex flex-row justify-between">
-                    <h5 className="mb-2 text-2xl font-light text-secondary tracking-tight text-primary">{name}</h5> 
-                    <div className="flex flex-row">
-                        <BedSvg className="w-6 m-2"/>
-                        <BedSvg className="w-6 m-2"/>
-                        <BedSvg className="w-6 m-2"/>
-                    </div>
-                </div>
-                <p className="mb-3 font-normal text-piction-blue">{detail}</p>
+      <Link
+        to={name}
+        state={{ name, detail, features, price }}
+        className="flex flex-col my-8 py-4 items-center bg-white border border-gray-200 bg-gray-100 hover:bg-gray-100"
+      >
+        <div className="flex flex-col justify-between p-4 leading-normal">
+          <div className="flex flex-row justify-between">
+            <h5 className="mb-2 text-2xl font-light text-secondary tracking-tight text-primary">
+              {name}
+            </h5>
+            <div className="flex flex-row">
+              <BedSvg className="w-6 m-2" />
+              <BedSvg className="w-6 m-2" />
+              <BedSvg className="w-6 m-2" />
             </div>
-        </Link>
+          </div>
+          <p className="mb-3 font-normal text-piction-blue">{detail}</p>
+        </div>
+      </Link>
     </div>
-           )
+  );
 }
 
-export default AparmentCard
+export default AparmentCard;
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,35 +1,55 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-import heroPicture from "../assets/home_hero_picture.jpg"
-import account from "../assets/account.svg"
+import heroPicture from "../assets/home_hero_picture.jpg";
+import account from "../assets/account.svg";
 
 function Home() {
-    return (
-        <div className="text-primary">
-            <div className="flex w-full justify-center items-center">
-                    <img className="object-cover max-w-full h-auto" src={heroPicture} alt="Pool picture"/>
-            </div>
-            <div className="container flex items-center justify-center py-4">
-                <p className="text-4xl py-4 px-8 text-center font-light">Uzivajte u odmoru s nama</p> 
-            </div>
-            <div className="container flex flex-wrap items-center justify-around p-2">
-                <Link to="/apartments">
-                    <button className="bg-secondary hover:bg-black text-white text-xl py-4 px-6 rounded-full">Book Now</button>
-                </Link>      
-            <div className="container flex flex-wrap items-center justify-around py-4 px-10">
-                <p className="text-4xl w-full text-center py-8 font-light">Your Hosts</p>
-                <img className="w-16" src={account} />
-                <img className="w-16" src={account} />
-            </div>
-            </div>
-            <div className="flex flex-wrap items-center justify-around bg-gray-100">
-                <div className="container text-center p-8">
-                    <p className="text-4xl w-full text-center font-light p-4">About Us</p>
-                    <p className="text-secondary p-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-                </div>
-            </div>
+  return (
+    <div className="text-primary">
+      <div className="flex w-full justify-center items-center">
+        <img
+          className="object-cover max-w-full h-auto"
+          src={heroPicture}
+          alt="Pool picture"
+        />
+      </div>
+      <div className="flex items-center justify-center p-8">
+        <p className="text-4xl py-4 px-8 text-center font-light">
+          Uzivajte u odmoru s nama
+        </p>
+      </div>
+      <div className="flex flex-wrap items-center justify-around p-4 w-full">
+        <div className="container w-full text-center">
+          <Link to="/apartments">
+            <button className="bg-secondary hover:bg-black text-white text-xl py-4 px-6 rounded-full">
+              Book Now
+            </button>
+          </Link>
         </div>
-           )
+      </div>
+      <div className="flex flex-wrap items-center justify-evenly w-full p-8 mt-8">
+        <p className="text-4xl w-full text-center font-light">Your Hosts</p>
+        <img className="w-16 py-8 my-8" src={account} />
+        <img className="w-16 py-8 my-8" src={account} />
+      </div>
+      <div className="flex flex-wrap items-center justify-around bg-gray-100">
+        <div className="container text-center p-8">
+          <p className="text-4xl w-full text-center font-light p-4">About Us</p>
+          <p className="text-secondary p-4">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
