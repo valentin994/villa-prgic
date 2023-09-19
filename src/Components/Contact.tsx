796b84e2bd333d644c89eb4460e1c6acca6b6266
valentin994 <valentin.vareskic@gmail.com>
2023-09-19 14:09:40 +0200

    added contact component

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,43 +1,24 @@
 function Contact() {
     return(
-    <div>
-        <form>
-            <div className="space-y-4 p-4">
-                <p className="w-full text-center text-2xl mt-4">Send a message to us</p>
-                <div className="grid sm:grid-cols-6">
-                    <div className="sm:col-span-4">
-                      <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
-                      <div className="mt-2">
-                        <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                          <input type="text" name="username" id="username"  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="John Doe" />
-                        </div>
-                      </div>
+        <section className="bg-white dark:bg-gray-900">
+            <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Interested in booking our accomodation, send us a message and check when it's free.</p>
+                <form action="#" className="space-y-8">
+                    <div>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                        <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="john.doe@gmail.com" required />
                     </div>
-                </div>
-                <div className="grid sm:grid-cols-6">
-                    <div className="sm:col-span-4">
-                      <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email</label>
-                      <div className="mt-2">
-                        <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                          <input type="text" name="email" id="email"  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="john.doe@email.com" />
-                        </div>
-                      </div>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                        <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
                     </div>
-                </div>
-                <div className="grid sm:grid-cols-6">
-                    <div className="sm:col-span-4">
-                      <label htmlFor="message" className="block text-sm font-medium leading-6 text-gray-900">Message</label>
-                      <div className="mt-2">
-                        <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                            <textarea id="about" name="about" rows={5} className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"></textarea>
-                        </div>
-                      </div>
-                    </div>
-                </div>
-            </div> 
-        </form>
-    </div>
-          )
+                    <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-secondary sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+                </form>
+            </div>
+            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">We'll try to answer ass soon as possible, you can also contact us on facebook, mobile or whatsapp</p>
+        </section>
+    )
 }
 
 export default Contact
