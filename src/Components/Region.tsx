192a6473c187f42401ac27bffd2f29af1cb73144
Valentin Vareskic <valentin.vareskic@gmail.com>
2023-10-26 15:00:15 +0200

    added visibilty hook for viewport

--- src/Components/Region.tsx
+++ src/Components/Region.tsx
@@ -7,65 +7,82 @@ function Region() {
     <div className="flex items-center justify-center p-8">
       <div className="container">
         <p className="text-secondary text-3xl text-center p-4">Region</p>
-        <p className="text-primary text-2xl p-4 text-piction-blue">Kanica</p>
-        <p className="text-primary p-4 text-secondary">
-          Kanica je turističko naselje smješteno u istoimenoj uvali udaljeno od
-          Rogoznice 4,2 km. Mjesto je idealno za miran, tih i ugodan odmor ali i
-          za sve one koji žele osjetiti toplu atmosferu privatnog smještaja.
-          Kanica za goste željne nešto aktivnijeg odmora nudi ronjenje, surfanje
-          ,ribolov , skijanje na vodi , ujedno je pogodna za pješačenje i vožnju
-          biciklom. Blizina mora i puno skrivenih uvala i plaža su joj dodatni
-          atributi. Plaže su pješčane,všljunčane i stjenovite s betoniranim
-          kupalištem pred kućama. Želite li savršeno mjesto za odmor sa vašom
-          obitelji i prijateljima i kućnim ljubimcima, sve pogodnosti ćete
-          pronaći u Kanici čiji mještani nude smještaj u apartmanima i sobama s
-          gostoprimstvom koji će učiniti Vaš odmor još ugodnijim. Dobrodošli
-          Kanica. Smještaj u Kanici. Apartmani, sobe, hoteli i ostali turistički
-          kapaciteti Kanica i okolica.
-        </p>
-        <img
-          src={KanicaImage}
-          alt="Kanica"
-          className="object-cover max-w-full h-auto p-4"
-        />
-        <p className="text-primary text-2xl p-4 text-piction-blue">Rogoznica</p>
-        <p className="text-primary p-4 text-secondary">
-          Najjužnije turističko središte- Rogoznica, sa susjednim mjestom
-          Ražanj, te s 54 km prekrasne razvedene obale, posebno je poznato po
-          nautičkom, obalnom i pomorskom turizmu. Malo, mirno i relativno mlado
-          naselje okruženo prekrasnim morskim krajolikom i otvorenim morem koje
-          oduzima dah, izgrađeno je na ruševinama živopisnih antičkih i
-          srednjovjekovnih naselja. O Rogoznici postoji i nekoliko mističnih i
-          fascinantnih legendi koje istraživača željnog putovanja povezuju s
-          primamljivim zovom čarobnih i dalekih mjesta.
-        </p>
-        <img
-          src={RogoznicaImage}
-          alt="Rogoznica"
-          className="object-cover max-w-full h-auto p-4"
-        />
-        <p className="text-primary text-2xl p-4 text-piction-blue">Trogir</p>
-        <p className="text-primary p-4 text-secondary">
-          Zahvaljujući svojem geografskom položaju Trogir je od pamtivijeka bio
-          idealno mjesto za život. S prirodno zaštićenom lukom, brojnim izvorima
-          pitke vode, plodnim tlom u zaleđu grada te s kamenom iz lokalnih
-          kamenoloma Trogir je neprekinuto nastanjen već više od 3600 godina.
-          Ovaj nadahnjujući mediteranski grad još je od vremena starih Grka
-          privlačio brojne majstore koji su ovdje živjeli i stvarali. Razni
-          umjetnici ovdje su pronašli inspiraciju i ostavili brojne tragove
-          svojeg djelovanja. Trogir je grad majstora, grad koji nas polako, ali
-          uvjerljivo, uvlači u začarani labirint ljepote, u uske kamene uličice
-          sazdane od žutoga glatkog kamena, toplih boja poslijepodnevnog sunca.
-          Ovaj je grad majstora svoju priču započeo portalom Majstora Radovana,
-          Muscardellom, Alešijem, Firentincem, Duknovićem i Blažom Jurjevom
-          Trogiraninom. Ti majstori označili su prekretnicu u povijesti Trogira,
-          smjestivši ga u sam svjetski umjetnički vrh.
-        </p>
-        <img
-          src={TrogirImage}
-          alt="Trogir"
-          className="object-cover max-w-full h-auto p-4"
-        />
+        <div className="grid grid-cols-3 border border-gray-50 rounded shadow my-8">
+          <div className="">
+            <p className="text-primary text-2xl p-4">Kanica</p>
+            <p className="text-primary p-4">
+              Kanica je turističko naselje smješteno u istoimenoj uvali udaljeno
+              od Rogoznice 4,2 km. Mjesto je idealno za miran, tih i ugodan
+              odmor ali i za sve one koji žele osjetiti toplu atmosferu
+              privatnog smještaja. Kanica za goste željne nešto aktivnijeg
+              odmora nudi ronjenje, surfanje ,ribolov , skijanje na vodi ,
+              ujedno je pogodna za pješačenje i vožnju biciklom. Blizina mora i
+              puno skrivenih uvala i plaža su joj dodatni atributi. Plaže su
+              pješčane,všljunčane i stjenovite s betoniranim kupalištem pred
+              kućama. Želite li savršeno mjesto za odmor sa vašom obitelji i
+              prijateljima i kućnim ljubimcima, sve pogodnosti ćete pronaći u
+              Kanici čiji mještani nude smještaj u apartmanima i sobama s
+              gostoprimstvom koji će učiniti Vaš odmor još ugodnijim. Dobrodošli
+              Kanica. Smještaj u Kanici.
+            </p>
+          </div>
+          <img
+            src={KanicaImage}
+            alt="Kanica"
+            className="object-cover max-w-full h-auto col-span-2"
+          />
+        </div>
+        <div className="grid grid-cols-3 border-1 border-gray shadow my-16">
+          <img
+            src={RogoznicaImage}
+            alt="Rogoznica"
+            className="object-cover max-w-full h-auto col-span-2"
+          />
+          <div className="">
+            <p className="text-primary text-2xl p-4 text-piction-blue">
+              Rogoznica
+            </p>
+            <p className="text-primary p-4 text-secondary">
+              Najjužnije turističko središte- Rogoznica, sa susjednim mjestom
+              Ražanj, te s 54 km prekrasne razvedene obale, posebno je poznato
+              po nautičkom, obalnom i pomorskom turizmu. Malo, mirno i relativno
+              mlado naselje okruženo prekrasnim morskim krajolikom i otvorenim
+              morem koje oduzima dah, izgrađeno je na ruševinama živopisnih
+              antičkih i srednjovjekovnih naselja. O Rogoznici postoji i
+              nekoliko mističnih i fascinantnih legendi koje istraživača željnog
+              putovanja povezuju s primamljivim zovom čarobnih i dalekih mjesta.
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-3 border-1 border-gray shadow my-16">
+          <div className="col-span-2">
+            <p className="text-primary text-2xl p-4 text-piction-blue">
+              Trogir
+            </p>
+            <p className="text-primary p-4 text-secondary">
+              Zahvaljujući svojem geografskom položaju Trogir je od pamtivijeka
+              bio idealno mjesto za život. S prirodno zaštićenom lukom, brojnim
+              izvorima pitke vode, plodnim tlom u zaleđu grada te s kamenom iz
+              lokalnih kamenoloma Trogir je neprekinuto nastanjen već više od
+              3600 godina. Ovaj nadahnjujući mediteranski grad još je od vremena
+              starih Grka privlačio brojne majstore koji su ovdje živjeli i
+              stvarali. Razni umjetnici ovdje su pronašli inspiraciju i ostavili
+              brojne tragove svojeg djelovanja. Trogir je grad majstora, grad
+              koji nas polako, ali uvjerljivo, uvlači u začarani labirint
+              ljepote, u uske kamene uličice sazdane od žutoga glatkog kamena,
+              toplih boja poslijepodnevnog sunca. Ovaj je grad majstora svoju
+              priču započeo portalom Majstora Radovana, Muscardellom, Alešijem,
+              Firentincem, Duknovićem i Blažom Jurjevom Trogiraninom. Ti
+              majstori označili su prekretnicu u povijesti Trogira, smjestivši
+              ga u sam svjetski umjetnički vrh.
+            </p>
+          </div>
+          <img
+            src={TrogirImage}
+            alt="Trogir"
+            className="object-cover max-w-full h-auto"
+          />
+        </div>
         <p className="text-primary text-2xl p-4 text-piction-blue">Split</p>
         <p className="text-primary p-4 text-secondary">
           Punih 17 stoljeća traje priča o Splitu, još otkako je rimski car
---
+++ src/utility/useIsVisible.tsx
@@ -0,0 +1,16 @@
+export function useIsVisible(): boolean {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIntersecting(entry.isIntersecting);
+    });
+
+    observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  return isIntersecting;
+}
