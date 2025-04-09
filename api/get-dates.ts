d5697af4a2ff0ba5786aa71c3fccfd943b9e40ad
valentin994 <valentin.vareskic@gmail.com>
2025-04-09 10:24:05 +0200

    adjusted env names

--- api/get-dates.ts
+++ api/get-dates.ts
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
   const { id } = req.query;
-  const apiKey = process.env.apikey;
+  const apikey = process.env.apikey;
   const url = process.env.url;
-  const response =  await fetch(`${url}${id}`, {headers:{"x-api-key": `${apiKey}`}})
+  const response =  await fetch(`${url}${id}`, {headers:{"x-api-key": `${apikey}`}})
   const data = await response.json();
   res.status(200).json(data);
 }
