133fa50b204a3a1dc19a0879bc283befebbb34d2
valentin994 <valentin.vareskic@gmail.com>
2025-04-09 10:39:27 +0200

    edge function redeploy

--- api/get-dates.ts
+++ api/get-dates.ts
@@ -3,6 +3,7 @@ export default async function handler(req, res) {
   const apikey = process.env.REACT_APP_API_KEY;
   const url = process.env.REACT_APP_API_URL;
   const response =  await fetch(`${url}${id}`, {headers:{"x-api-key": `${apikey}`}})
+  return response
   const data = await response.json();
   res.status(200).json(data);
 }
